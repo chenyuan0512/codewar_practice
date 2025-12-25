a9b7eee763fc80d95bb4edd11986959aed599bde
CYY <qqog3791@gmail.com>
2025-12-25 20:03:05 +0800

    4kyu, hamming number

--- practice.py
+++ practice.py
@@ -1,35 +1,26 @@
-def next_bigger(n):
-    n = [i for i in str(n)]
+def hamming(n):
+    ans_list = [1]
+    power_2, power_3, power_5 = 0, 0, 0
 
-    index = len(n) - 1
+    for i in range(n-1):
 
-    # 從個位和十位開始比
-    # 如果個位比十位大
-    #     就交換位置
-    # 沒有比較大(一樣或小)
-    #     維持原樣
-    # 回到十位和百位比，重複下去直到最高位
+        next_r2 = ans_list[power_2] * 2
+        next_r3 = ans_list[power_3] * 3
+        next_r5 = ans_list[power_5] * 5
+        next_row_list = [next_r2, next_r3, next_r5]
 
-    while index > 0:
-        if n[index-1] < n[index]:  # 從後面看回來，右邊比較大
-            turning_points = n[index-1]
+        smallest = min(next_row_list)
 
-            min_point = max(n[index:])
+        ans_list.append(smallest)
 
-            for i in n[index:]:
-                if i > turning_points and i < min_point:
-                    print(i, min_point)
-                    min_point = i
+        if next_r2 == smallest:
+            power_2 += 1
+        if next_r3 == smallest:
+            power_3 += 1
+        if next_r5 == smallest:
+            power_5 += 1
 
-            min_point_index = n[index:].index(min_point) + index
-
-            n[index-1] = min_point
-            n[min_point_index] = turning_points
-
-            n[index:] = sorted(n[index:])
-            return int("".join(n))
-        index -= 1
-    return -1
+    return ans_list[-1]
 
 
-print(next_bigger(414))
+print(hamming(5000))
