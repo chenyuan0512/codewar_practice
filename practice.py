0b3e40625db988a2a6e59baddfe943bb2090a5c8
CYY <qqog3791@gmail.com>
2026-02-10 16:44:38 +0800

    3 kyu，Line Safari，還沒寫完

--- practice.py
+++ practice.py
@@ -1,94 +1,92 @@
-class Dinglemouse(object):
-    def __init__(self, queues, capacity):
-        self.queues = queues  # queues 是所有樓層所有人要去哪一層的 info
-        self.capacity = capacity  # 電梯最多可以載幾個人
-        self.current_floor = 0  # 目前樓層
-        self.lifted_passengers_list = []
+class LineSafari:
+    def __init__(self, grid):
+        self.grid = grid
+        self.direction = [0, 1] # y, x 格式和 _find_X 一樣
+        self.CORNER = "+"
+        self.HOR = "-"
+        self.VER = "|"
+        
+        self.valid_horizontal = {self.HOR, self.CORNER} # 記得 X 也是合法的連接點
+        self.valid_vertical = {self.VER, self.CORNER}
+    
+    # 這邊只要先找到第一個就好，因為不管從哪一個開始都會走到下一個
+    def _find_X(self):
+        for i in range(len(self.grid)):
+            if "X" in self.grid[i]:
+                self.y, self.x = i, self.grid[i].index("X")
+                # return [i, self.grid[i].index("X")]
+    
+
+    def _find_lines_around(self):
+        # self.y, self.x = pos[0], pos[1]
+        y_up = self.y if self.y == 0 else self.y - 1
+        y_down = self.y if self.y == len(self.grid) else self.y + 1
+
+        x_left = self.x if self.x == 0 else self.x - 1
+        x_right = self.x if self.x == len(self.grid) else self.x + 1
+
+        neighbor_list = [self.grid[y_up][self.x], self.grid[y_down][self.x], self.grid[self.y][x_left], self.grid[self.y][x_right]]
+        # [上、下、左、右]
+
+
+
+        return neighbor_list
+
+
+    def _walk_around(self):
+        # 1. 找 X 位置: 只要第一次找完0就好
+        # 這個應該從外面傳進來
+
+        # 2. 找邊界: 先定義出上下左右 index
+        next_round = self._find_lines_around()
+        
+        # 3. 找周圍: 看上下左右有沒有 symbol
+        recurred_list = []
+        
+        for i in range(len(next_round)):
+            if next_round[i] in self.valid_vertical and i < 2:
+                valid = next_round[i]
+                y = -1 if i == 0 else 1
+                x = 0
+                recurred_list.append(valid)
+                self.direction = [y, x]
+            
+            elif next_round[i] in self.valid_horizontal and 1 < i:
+                valid = next_round[i]
+                x = -1 if i == 2 else 1
+                y = 0
+                recurred_list.append(valid)
+                self.direction = [y, x]
+        
+        # 這邊要 return 的應該是 list + 座標
+        
+        self.y += self.direction[0]
+        self.x += self.direction[0]
+        return recurred_list
+    
+    
+
+    def run(self):
+        self._find_X()
+        self._walk_around()
+
         
 
-        self.visited_list = [self.current_floor]
-        self.direction = 1
-
-    # 先把 queue 從 tuple 轉 list
-    # 因為 tuple 是固定值，不能更動
-
-    def _tuple_to_index(self):
-        self.queues_list = [list(i) for i in self.queues]
-        # self.queues_list_copy = self.queues_list.copy()
-
-    # 出去
-    def _leave(self):
-        self.lifted_passengers_list = [
-            p for p in self.lifted_passengers_list if p != self.current_floor]
-        self._record_visited_floor()
-
-    # 紀錄目前樓層
-    def _record_visited_floor(self):
-        if self.current_floor != self.visited_list[-1]:
-            self.visited_list.append(self.current_floor)
-
-    def _next_floor(self):
-        while True:
-            print(self.visited_list)
-            empty = 0
-            for i in self.queues_list:
-                if i == []:
-                    empty += 1
-                if empty == len(self.queues_list) and self.lifted_passengers_list == []:
-                    self.current_floor = 0
-                    self._record_visited_floor()
-                    return self.visited_list
-
-            # 現在改成每個樓層都停下來檢查
-
-            # 檢查有沒有人要出去
-            if self.current_floor in self.lifted_passengers_list:
-                self._leave()
-
-            # 檢查有沒有人要進來
-            for target_floor in list(self.queues_list[self.current_floor]):
-                if (self.direction > 0 and target_floor > self.current_floor) or (self.direction < 0 and target_floor < self.current_floor):
-                    if len(self.lifted_passengers_list) < self.capacity:
-                        self.lifted_passengers_list.append(target_floor)
-                        self.queues_list[self.current_floor].remove(target_floor)
-                    self._record_visited_floor()
-            
-
-            self.current_floor += self.direction
-
-            if self.current_floor >= len(self.queues_list) - 1:
-                self.current_floor = len(self.queues_list) - 1
-                self.direction = -1
-
-            elif self.current_floor <= 0:
-                self.current_floor = 0
-                self.direction = 1
 
 
-    def theLift(self):
-        self._tuple_to_index()
-        visited = self._next_floor()
-        # print("current floor", self.current_floor)
-        # print("target_floor: ", self.target_floor_list)
-        # print("number_inside_elevator: ", self.lift_passengers_list)
-        print("visited: ", visited)
-
-        # print(self.queues)
-        # self._next_floor()
-        return visited
+        # 先檢查四周有幾條線
 
 
-# Floors:    G     1      2        3     4      5      6         Answers:
-tests = [
-    # [((),   (),    (5, 5, 5), (),   (),    (),    ()),     [0, 2, 5, 0]],
-    # [((),   (),    (1, 1),   (),   (),    (),    ()),     [0, 2, 1, 0]],
-    # [((),   (3,),  (4,),    (),   (5,),  (),    ()),     [0, 1, 2, 3, 4, 5, 0]],
-    [((),   (0,),  (),      (),   (2,),  (3,),  ()),     [0, 5, 4, 3, 2, 1, 0]],
-    # [((),   (6, 6, 6, 5, 0, 0, 0, 0, 0, 0,),  (),  (),   (),
-    #   (1, 1, 1, 1, 0, ),  (5,)),     [0, 5, 4, 3, 2, 1, 0]]
-]
 
-for queues, answer in tests:
-    # print(queues)
-    lift = Dinglemouse(queues, 5)
-    lift.theLift()
+def line(grid):
+    
+    line = LineSafari(grid)
+    print(line.run())
+
+grid = [
+        "   |--------+    ",
+        "X---        ---+ ",
+        "               | ",
+        "               X "
+    ]
+line(grid)
