a5577230040724a9f9c35f6ed5b08c81fd2b859a
CYY <qqog3791@gmail.com>
2026-02-11 13:36:36 +0800

    3 kyu，Line Safari 第三版，還沒寫完

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 class LineSafari:
     def __init__(self, grid):
         self.grid = grid
-        self.direction = [0, 1]  # y, x 格式和 _find_X 一樣
+        self.direction = (0, 1)  # y, x 格式和 _find_X 一樣
         self.CORNER = "+"
         self.HOR = "-"
         self.VER = "|"
@@ -13,6 +13,8 @@ class LineSafari:
         # 先確認不會超過邊界
         self.height = len(self.grid)
         self.width = len(self.grid[0])
+        self.seen_x_times = 0
+        self.previous_sign = " "
 
     # 這邊只要先找到第一個就好，因為不管從哪一個開始都會走到下一個
 
@@ -21,7 +23,8 @@ class LineSafari:
             if "X" in self.grid[i]:
                 self.y, self.x = i, self.grid[i].index("X")
                 self.start_point = (self.y, self.x)
-                # return [i, self.grid[i].index("X")]
+                self.previous_sign = "X"
+                return # 如果沒有 return，會繼續找下一個
 
     def _find_lines_around(self):
         # self.y, self.x = pos[0], pos[1]
@@ -30,61 +33,94 @@ class LineSafari:
 
         x_left = self.x if self.x == 0 else self.x - 1
         x_right = self.x if self.x == len(self.grid) else self.x + 1
-
-        neighbor_list = [self._get_char(y_up, self.x), self._get_char(
-            y_down, self.x), self._get_char(self.y, x_left), self._get_char(self.y, x_right)]
+        print(y_up, y_down, x_left, x_right)
+        print("center:", self.y, self.x)
+        neighbor_list = [self._get_char(y_up, self.x), self._get_char(y_down, self.x), self._get_char(self.y, x_left), self._get_char(self.y, x_right)]
+        print("neighbor list:", neighbor_list)
         # [上、下、左、右]
 
         return neighbor_list
+    
+    def _is_valid(self, r, c):
+        return 0 <= r < self.height and 0 <= c < self.width
+
+    def _get_char(self, r, c):
+        if self._is_valid(r, c):
+            return self.grid[r][c]
 
     def _walk_around(self):
+        
         # 1. 找 X 位置: 只要第一次找完0就好
         # 這個應該從外面傳進來
 
-        # 2. 找邊界: 先定義出上下左右 index
+        # 2. 找邊界: 先定義出上下、左右 index，這邊把 x 和 y 分開，統一先走 y
         next_round = self._find_lines_around()
 
         # 3. 找周圍: 看上下左右有沒有 symbol
         recurred_list = []
 
-        for i in range(len(next_round)):
-            next = self._get_char()
-            if next_round[i] in self.valid_vertical and i < 2 and (self.y, self.x) != self.start_point:
-                valid = next_round[i]
-                y = -1 if i == 0 else 1
-                x = 0
-                recurred_list.append(valid)
-                self.direction = [y, x]
+        for i in range(len(next_round)): # 因為在 self._find_lines_around 已經檢查過了，所以這邊不用再檢查一次
+            y, x = 0, 0
+            next = next_round[i]
+            
+            if next == " ":
+                continue
+            elif next == "X" and (self.y, self.x) == self.start_point:
+                self.seen_x_times += 1
+                continue
 
-            elif next_round[i] in self.valid_horizontal and 1 < i and (self.y, self.x) != self.start_point:
-                valid = next_round[i]
-                x = -1 if i == 2 else 1
-                y = 0
-                recurred_list.append(valid)
-                self.direction = [y, x]
+            elif i < 2:  
+                if next == "-":
+                    return "Fail"
+                else:
+                    y = -1 if i == 0 else 1
+                    x = 0
+            elif 1 < i:
+                if next == "|":
+                    return "Fail"
+                else:
+                    print(next)
+                    x = -1 if i == 2 else 1
+                    y = 0
 
-        # 這邊要 return 的應該是 list + 座標
+            self.direction = (y, x)
+            self.y += self.direction[0]
+            self.x += self.direction[1]
+            
+            return "Ok"
 
-        self.y += self.direction[0]
-        self.x += self.direction[1]
+        #     if next in self.valid_vertical and i < 2 and (self.y, self.x) != self.start_point:
+        #         y = -1 if i == 0 else 1
+        #         x = 0
+        #         recurred_list.append(next)
+        #         self.direction = [y, x]
+        #     elif next in self.valid_horizontal and 1 < i and (self.y, self.x) != self.start_point:
+        #         x = -1 if i == 2 else 1
+        #         y = 0
+        #         recurred_list.append(next)
+        #         self.direction = [y, x]
 
-        return recurred_list
+        # # 這邊要 return 的應該是 list + 座標
 
-    def _is_valid(self, r, c):
-        return 0 <= r < self.height and 0 <= c < self.width
+        # self.y += self.direction[0]
+        # self.x += self.direction[1]
 
-    def _get_char(self, r, c):
-        if self.is_valid(r, c):
-            return self.grid[r][c]
-        return None
+        # return recurred_list
+
+    
+        # return None
 
     def run(self):
         self._find_X()
-        neighbor_list = self._walk_around()
 
-        while "X" not in neighbor_list:
-            neighbor_list = self._walk_around()
-            print(neighbor_list)
+        result = self._walk_around()
+        
+        while self.seen_x_times < 2 and result == 'Ok':
+            result = self._walk_around()
+
+        # while "X" not in neighbor_list:
+        #     neighbor_list = self._walk_around()
+        #     print(neighbor_list)
 
         # 先檢查四周有幾條線
 
@@ -92,8 +128,8 @@ class LineSafari:
 def line(grid):
 
     line = LineSafari(grid)
-    print(line.height, line.width)
-    # print(line.run())
+    # print(line.height, line.width)
+    print(line.run())
 
 
 grid = [
